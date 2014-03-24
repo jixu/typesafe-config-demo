b153c32e734df933da58abad357f83e80df683c1
jixu <jixuxjixu@gmail.com>
2014-03-24 21:27:26 +0800

    new requirement

--- src/main/scala/slingstone/Demo.scala
+++ src/main/scala/slingstone/Demo.scala
@@ -8,22 +8,36 @@ import scala.collection.JavaConversions._
  */
 
 case class MatchTypeConfig(name: String, candidateNum: Int, usePrediction: Boolean)
-case class BlenderConfig(useDedup: Boolean, useVariety: Boolean)
-case class FederationConfig(matchTypes: Seq[MatchTypeConfig])
-case class BucketConfig(name: String, federationConfig: FederationConfig, blenderConfig: BlenderConfig)
+case class UpServiceConfig(name: String, keys: String)
+
+case class FederationConfig(predictionModel: String, predictionDocLimit: Int, matchTypes: Seq[MatchTypeConfig])
+case class UserProfileConfig(upServices: Seq[UpServiceConfig])
+case class BucketConfig(name: String, ccode: String, federationConfig: FederationConfig, userProfileConfig: UserProfileConfig)
 
 object Demo extends App {
-  println(createBucketConfigFromFile("demo.conf"))
-  println(createBucketConfigFromFile("inherit.conf"))
-  println(createBucketConfigFromFile("inherit_again.conf"))
+  println(createBucketConfigFromFile("base.conf"))
+  println(createBucketConfigFromFile("changeModel.conf"))
+  println(createBucketConfigFromFile("overrideMatchTypes.conf"))
+  println(createBucketConfigFromFile("overrideUpServices.conf"))
 
   def createBucketConfigFromFile(fileName: String): BucketConfig = {
     val conf = ConfigFactory.parseResources(fileName)
-    val blenderConfig = BlenderConfig(conf.getBoolean("blender.useDedup"), conf.getBoolean("blender.useVariety"))
+
+    val name = conf.getString("name")
+    val ccode = conf.getString("ccode")
+
     val matchTypeConfigs = conf.getConfigList("federation.matchTypes") map { config =>
       MatchTypeConfig(config.getString("name"), config.getInt("candidateNum"), config.getBoolean("usePrediction"))
     }
-    val federationConfig = FederationConfig(matchTypeConfigs)
-    BucketConfig(conf.getString("name"), federationConfig, blenderConfig)
+    val predictionModel = conf.getString("federation.predictionModel")
+    val predictionDocLimit = conf.getInt("federation.predictionDocLimit")
+    val federationConfig = FederationConfig(predictionModel, predictionDocLimit, matchTypeConfigs)
+
+    val upServiceConfigs = conf.getConfigList("userprofile.upServices") map { config =>
+      UpServiceConfig(config.getString("name"), config.getString("keys"))
+    }
+    val userProfileConfig = UserProfileConfig(upServiceConfigs)
+
+    BucketConfig(name, ccode, federationConfig, userProfileConfig)
   }
 }
